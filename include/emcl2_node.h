5545012b9ff53b956360e780a6fba1326d279021
Ryuichi Ueda <ryuichiueda@gmail.com>
2022-12-27 10:07:19 +0900

    Split code into .cpp and .h

---
+++ include/emcl2_node.h
@@ -0,0 +1,37 @@
+//SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
+//SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef INTERFACE_EMCL2_H__
+#define INTERFACE_EMCL2_H__
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+namespace emcl2 {
+
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+	MinimalPublisher();
+
+  private:
+	void timer_callback();
+	
+	rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+	size_t count_;
+};
+
+}
+
+#endif
--- src/emcl2_node.cpp
+++ src/emcl2_node.cpp
@@ -1,3 +1,8 @@
+#include "emcl2_node.h"
+
+namespace emcl2 {
+
+/*
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -8,37 +13,43 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
 
 class MinimalPublisher : public rclcpp::Node
 {
   public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
-    {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-    }
+	MinimalPublisher();
 
   private:
-    void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+	void timer_callback();
+	
+	rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+	size_t count_;
 };
+*/
+
+MinimalPublisher::MinimalPublisher() : Node("minimal_publisher"), count_(0)
+{
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    timer_ = this->create_wall_timer(
+    500ms, std::bind(&MinimalPublisher::timer_callback, this));
+}
+
+void MinimalPublisher::timer_callback()
+{
+	auto message = std_msgs::msg::String();
+	message.data = "Hello, world! " + std::to_string(count_++);
+	RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+	publisher_->publish(message);
+}
+
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<emcl2::MinimalPublisher>());
   rclcpp::shutdown();
   return 0;
 }
+
