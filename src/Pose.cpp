1a6564ff77c27d53e02107103850224573d3df02
AtsukiUsui <kmmm13037@gmail.com>
2023-05-13 22:31:21 +0900

    Porting/header file path of Pose

--- src/Pose.cpp
+++ src/Pose.cpp
@@ -1,16 +1,14 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
-#include "emcl/Pose.h"
-#include <sstream>
+#include "emcl2/Pose.h"
+
 #include <cmath>
+#include <sstream>
 
-namespace emcl2 {
-
-Pose::Pose(double x, double y, double t)
+namespace emcl2
 {
-	set(x, y, t);
-}
+Pose::Pose(double x, double y, double t) { set(x, y, t); }
 
 void Pose::set(double x, double y, double t)
 {
@@ -19,7 +17,7 @@ void Pose::set(double x, double y, double t)
 	t_ = t;
 }
 
-void Pose::set(const Pose &p)
+void Pose::set(const Pose & p)
 {
 	x_ = p.x_;
 	y_ = p.y_;
@@ -35,13 +33,11 @@ std::string Pose::to_s(void)
 
 void Pose::normalizeAngle(void)
 {
-	while(t_ > M_PI)
-		t_ -= 2*M_PI;
-	while(t_ < -M_PI)
-		t_ += 2*M_PI;
+	while (t_ > M_PI) t_ -= 2 * M_PI;
+	while (t_ < -M_PI) t_ += 2 * M_PI;
 }
 
-Pose Pose::operator -(const Pose &p) const
+Pose Pose::operator-(const Pose & p) const
 {
 	Pose ans(x_ - p.x_, y_ - p.y_, t_ - p.t_);
 	ans.normalizeAngle();
@@ -49,48 +45,40 @@ Pose Pose::operator -(const Pose &p) const
 	return ans;
 }
 
-Pose Pose::operator =(const Pose &p)
+Pose Pose::operator=(const Pose & p)
 {
 	x_ = p.x_;
-       	y_ = p.y_;
+	y_ = p.y_;
 	t_ = p.t_;
 	return *this;
 }
 
-void Pose::move(double length, double direction, double rotation,
-		double fw_noise, double rot_noise)
+void Pose::move(double length, double direction, double rotation, double fw_noise, double rot_noise)
 {
-	x_ += (length + fw_noise)*cos(direction + rot_noise + t_);
-	y_ += (length + fw_noise)*sin(direction + rot_noise + t_);
+	x_ += (length + fw_noise) * cos(direction + rot_noise + t_);
+	y_ += (length + fw_noise) * sin(direction + rot_noise + t_);
 	t_ += rotation + rot_noise;
 	normalizeAngle();
 }
 
-bool Pose::nearlyZero(void)
-{
-	return fabs(x_) < 0.001 and fabs(y_) < 0.001 and fabs(t_) < 0.001;
-}
+bool Pose::nearlyZero(void) { return fabs(x_) < 0.001 and fabs(y_) < 0.001 and fabs(t_) < 0.001; }
 
 uint16_t Pose::get16bitRepresentation(void)
 {
-	int tmp = t_/M_PI*(1<<15);
-	while(tmp < 0)
-		tmp += (1<<16);
-	while(tmp >= (1<<16))
-		tmp -= (1<<16);
+	int tmp = t_ / M_PI * (1 << 15);
+	while (tmp < 0) tmp += (1 << 16);
+	while (tmp >= (1 << 16)) tmp -= (1 << 16);
 
 	return (uint16_t)tmp;
 }
 
 uint16_t Pose::get16bitRepresentation(double t)
 {
-	int tmp = t/M_PI*(1<<15);
-	while(tmp < 0)
-		tmp += (1<<16);
-	while(tmp >= (1<<16))
-		tmp -= (1<<16);
+	int tmp = t / M_PI * (1 << 15);
+	while (tmp < 0) tmp += (1 << 16);
+	while (tmp >= (1 << 16)) tmp -= (1 << 16);
 
 	return (uint16_t)tmp;
 }
 
-}
+}  // namespace emcl2
