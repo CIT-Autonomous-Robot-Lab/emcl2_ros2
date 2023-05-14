6a756f29bb29877cfc816665b11241b5453f7d94
Atsuki Usui <kmmm13037@gmail.com>
2023-05-14 15:40:05 +0900

    Porting/exp reset mcl2 (#17)
    * Porting/header file pass of ExpResetMcl2
    
    * Porting/RCLCPP_INFO of ExpResetMcl2

--- include/emcl2/ExpResetMcl2.h
+++ include/emcl2/ExpResetMcl2.h
@@ -4,24 +4,23 @@
 #ifndef EXP_PF2_H__
 #define EXP_PF2_H__
 
-#include "emcl/Mcl.h"
+#include "emcl2/Mcl.h"
 
-namespace emcl2 {
-
+namespace emcl2
+{
 class ExpResetMcl2 : public Mcl
 {
-public: 
-	ExpResetMcl2(const Pose &p, int num, const Scan &scan,
-			const std::shared_ptr<OdomModel> &odom_model,
-			const std::shared_ptr<LikelihoodFieldMap> &map,
-			double alpha_th,
-			double expansion_radius_position, double expansion_radius_orientation,
-			double extraction_rate, double successive_penetration_threshold,
-			bool sensor_reset);
+      public:
+	ExpResetMcl2(
+	  const Pose & p, int num, const Scan & scan, const std::shared_ptr<OdomModel> & odom_model,
+	  const std::shared_ptr<LikelihoodFieldMap> & map, double alpha_th,
+	  double expansion_radius_position, double expansion_radius_orientation,
+	  double extraction_rate, double successive_penetration_threshold, bool sensor_reset);
 	~ExpResetMcl2();
 
 	void sensorUpdate(double lidar_x, double lidar_y, double lidar_t, bool inv);
-private:
+
+      private:
 	double alpha_threshold_;
 	double expansion_radius_position_;
 	double expansion_radius_orientation_;
@@ -32,10 +31,10 @@ private:
 
 	void expansionReset(void);
 
-//bool Particle::isPenetrating(
-	double nonPenetrationRate(int skip, LikelihoodFieldMap *map, Scan &scan);
+	//bool Particle::isPenetrating(
+	double nonPenetrationRate(int skip, LikelihoodFieldMap * map, Scan & scan);
 };
 
-}
+}  // namespace emcl2
 
 #endif
