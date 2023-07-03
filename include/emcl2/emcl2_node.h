1b53a0ad4af623ef808072f8e456e8fb7f386663
IkuoShige <85383887+IkuoShige@users.noreply.github.com>
2023-07-03 21:16:18 +0900

    Fix tf support for ROS 2 in emcl2_node (#21)
    * Fix tf support for ROS 2 in emcl2_node
    
    * Fix transform_expiration in transform_tolerance_
    
    * Remove comment out from emcl2_node
    
    * Remove unused code and RCLCPP_INFO for cbScan

--- include/emcl2/emcl2_node.h
+++ include/emcl2/emcl2_node.h
@@ -1,21 +1,15 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
-//CAUTION: Some lines came from amcl (LGPL). These lines are commented out now.
+//CAUTION: Some lines came from amcl (LGPL).
 
 #ifndef INTERFACE_EMCL2_H__
 #define INTERFACE_EMCL2_H__
 
-// #include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 
 #include "emcl2/ExpResetMcl2.h"
 #include "emcl2/LikelihoodFieldMap.h"
 #include "emcl2/OdomModel.h"
-
-/* came from amcl (LGPL). But these lines will be the same even if anyone creates. 
-#include "tf2_ros/message_filter.h"
-#include "tf2_ros/transform_broadcaster.h"
-#include "tf2_ros/transform_listener.h"
-*/
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -24,6 +18,9 @@
 #include "std_msgs/msg/float32.hpp"
 #include "std_srvs/srv/empty.hpp"
 #include "tf2/LinearMath/Transform.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/transform_listener.h"
 
 namespace emcl2
 {
@@ -39,9 +36,6 @@ class EMcl2Node : public rclcpp::Node
 
       private:
 	std::shared_ptr<ExpResetMcl2> pf_;
-
-	//ros::NodeHandle nh_;
-	//ros::NodeHandle private_nh_;
 
 	rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr particlecloud_pub_;
 	rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_pub_;
@@ -61,17 +55,19 @@ class EMcl2Node : public rclcpp::Node
 	std::string scan_frame_id_;
 	std::string base_frame_id_;
 
-	/* came from amcl. 
 	std::shared_ptr<tf2_ros::TransformBroadcaster> tfb_;
 	std::shared_ptr<tf2_ros::TransformListener> tfl_;
 	std::shared_ptr<tf2_ros::Buffer> tf_;
 
 	tf2::Transform latest_tf_;
-	*/
+
+	rclcpp::Clock ros_clock_;
 
 	int odom_freq_;
 	bool init_request_;
+	bool initialpose_receive_;
 	bool simple_reset_request_;
+	bool scan_receive_;
 	bool map_receive_;
 	double init_x_, init_y_, init_t_;
 
@@ -86,6 +82,7 @@ class EMcl2Node : public rclcpp::Node
 	void receiveMap(nav_msgs::msg::OccupancyGrid::ConstSharedPtr msg);
 
 	void initCommunication(void);
+	void initTF();
 	void initPF(void);
 	std::shared_ptr<LikelihoodFieldMap> initMap(void);
 	std::shared_ptr<OdomModel> initOdometry(void);
