fc5489f9cba75fbe1fa5dd90b8248c0909c36d04
Tatsuhiro Ikebe <beike315@icloud.com>
2023-07-19 21:33:14 +0900

    Feature quick start (#37)
    * Add map_server
    
    * Add dependent nav2_common
    
    * Add emcl2_quick_start.param.yaml
    
    * Fix initial_pose_x
    
    * Remove dependent pkg nav2_common
    
    * Refactor
    
    * Feature quick start

--- README.md
+++ README.md
@@ -83,6 +83,28 @@ The followings have never been implemented yet.
 
 * ROS 2 Humble Hawksbill
 
+## quick start
+
+### Install & Build
+```
+mkdir catkin_ws && cd catkin_ws
+git clone git@github.com:CIT-Autonomous-Robot-Lab/emcl2_ros2.git ./src/emcl2_ros2
+rosdep update
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+sudo apt install -y ros-$ROS_DISTRO-navigation2 ros-$ROS_DISTRO-nav2-bringup ros-$ROS_DISTRO-turtlebot3-gazebo
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+source install/setup.bash
+```
+
+### Try emcl2 in simulator
+```
+export TURTLEBOT3_MODEL=burger
+ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
+ros2 launch nav2_bringup rviz_launch.py
+ros2 launch emcl2 emcl2.launch.py params_file:=$(ros2 pkg prefix --share emcl2)/config/emcl2_quick_start.param.yaml map:=$(ros2 pkg prefix --share nav2_bringup)/maps/turtlebot3_world.yaml use_sim_time:=true
+ros2 launch nav2_bringup navigation_launch.py use_sim_time:=true
+```
+
 ## To do
 
 * Implement service
--- launch/emcl2.launch.py
+++ launch/emcl2.launch.py
@@ -3,25 +3,64 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import GroupAction
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch_ros.actions import Node, SetParameter
 
 
 def generate_launch_description():
-    emcl2_dir = get_package_share_directory('emcl2')
-    params_file = os.path.join(
-        emcl2_dir, 'config', 'emcl2.param.yaml')
+    params_file = LaunchConfiguration('params_file')
+    map_yaml_file = LaunchConfiguration('map')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-    launch_node = GroupAction([
-        Node(
-            name='emcl2',
-            package='emcl2',
-            executable='emcl2_node',
-            parameters=[params_file],
-            output='screen')
-    ])
+    declare_map_yaml = DeclareLaunchArgument(
+        'map',
+        default_value='',
+        description='Full path to map yaml file to load')
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+    declare_params_file = DeclareLaunchArgument(
+        'params_file',
+        default_value=[
+            TextSubstitution(text=os.path.join(
+                get_package_share_directory('emcl2'), 'config', '')),
+            TextSubstitution(text='emcl2.param.yaml')],
+        description='emcl2 param file path')
+
+    lifecycle_nodes = ['map_server']
+
+    launch_node = GroupAction(
+        actions=[
+            SetParameter('use_sim_time', use_sim_time),
+            Node(
+                package='nav2_map_server',
+                executable='map_server',
+                name='map_server',
+                parameters=[{'yaml_filename': map_yaml_file}],
+                output='screen'),
+            Node(
+                name='emcl2',
+                package='emcl2',
+                executable='emcl2_node',
+                parameters=[params_file],
+                output='screen'),
+            Node(
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager_localization',
+                output='screen',
+                parameters=[{'autostart': True},
+                            {'node_names': lifecycle_nodes}])
+        ]
+    )
 
     ld = LaunchDescription()
+    ld.add_action(declare_map_yaml)
+    ld.add_action(declare_use_sim_time)
+    ld.add_action(declare_params_file)
+
     ld.add_action(launch_node)
 
     return ld
