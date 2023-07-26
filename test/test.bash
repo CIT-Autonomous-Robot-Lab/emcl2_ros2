b93d8d235409652db4d5d5b145f9c438e5fb5e77
Tatsuhiro Ikebe <beike315@icloud.com>
2023-07-26 19:24:13 +0900

    Feature ros2 action send_goal /navigate_to_pose

--- test/test.bash
+++ test/test.bash
@@ -43,15 +43,15 @@ sleep 10
 
 ### NAVIGATION TEST ###
 # Publish nav2 goal
-ros2 topic pub /goal_pose geometry_msgs/msg/PoseStamped "
-header:
-  stamp:
-    sec: 0
-    nanosec: 0
-  frame_id: 'map'
+ros2 action send_goal /navigate_to_pose nav2_msgs/action/NavigateToPose "
 pose:
-  position: {x: -0.5, y: -0.5, z: 0.0}
-  orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}" | head -n 5
+  header: 
+    stamp: 
+      sec: 0
+    frame_id: 'map'
+  pose:
+    position: { x: -0.5, y: -0.5, z: 0.0}
+    orientation: { x: 0.0, y: 0.0, z: 0.0, w: 1.0}" &
 
 # Check if position is within a threshold
 ros2 topic echo /mcl_pose --csv | 
