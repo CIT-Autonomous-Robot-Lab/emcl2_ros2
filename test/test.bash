a0bbc8b0b6c16cc7386b7f44accc88be8073e3e8
Tatsuhiro Ikebe <beike315@icloud.com>
2023-08-16 10:25:29 +0900

    Fix test yml workflow event trigger pull request (#44)
    * Cut sensor resetting off at default
    
    * Remove unnecessary command execution
    
    * Fix test.yml workflow event trigger pull-request
    
    * Fix printf test result
    
    * Add sleep before launch emcl2
    
    * Fix test.yml workflow event trigger
    
    * Fix test.yml workflow event trigger branch main
    
    * Fix executable launch emcl2
    
    * Feature ros2 action send_goal /navigate_to_pose
    
    * Add sleep
    
    * Fix sleep time
    
    * Remove sleep
    
    * CI Test
    
    * CI Test 2
    
    * CI Test 3
    
    * CI Test 4
    
    * CI Test 5
    
    * CI Test 6
    
    * CI Test 7
    
    * CI Test 8
    
    * CI Test 9
    
    * CI Test 10
    
    * Fix laser_likelihood_max_dist
    
    * Fix sleep time
    
    * CI Test 1
    
    * CI Test 2
    
    * CI Test 3
    
    * CI Test 4
    
    * CI Test 5
    
    * CI Test 6
    
    * CI Test 7
    
    * CI Test 8
    
    * CI Test 9
    
    * CI Test 10
    
    * Remove ros2 daemon
    
    * Fix launch order
    
    * CI Test 1
    
    * CI Test 2
    
    * CI Test 3
    
    * CI Test 4
    
    * CI Test 5
    
    * CI Test 6
    
    * CI Test 7
    
    * CI Test 8
    
    * CI Test 9
    
    * CI Test 10
    
    * Feature canTransform footprint odom
    
    * Fix sleep time
    
    * Add sleep
    
    * CI Test 1
    
    * CI Test 2
    
    * CI Test 3
    
    * CI Test 4
    
    * CI Test 5
    
    * CI Test 6
    
    * CI Test 7
    
    * CI Test 8
    
    * CI Test 9
    
    * CI Test 10
    
    * Fix canTransform time
    
    * CI Test 1
    
    * CI Test 2
    
    * CI Test 3
    
    * CI Test 4
    
    * CI Test 5
    
    * CI Test 6
    
    * CI Test 7
    
    * CI Test 8
    
    * CI Test 9
    
    * CI Test 10
    
    * Remove canTransform
    
    * CI TEST 1
    
    * CI TEST 2
    
    * CI TEST 3
    
    * CI TEST 3
    
    * CI TEST 5
    
    * CI TEST 6
    
    * CI TEST 7
    
    * CI TEST 8
    
    * CI TEST 9
    
    * CI TEST 10

--- test/test.bash
+++ test/test.bash
@@ -30,8 +30,8 @@ pose:
 ros2 topic echo /mcl_pose --csv | 
 head -n 1000 |
 awk -F',' '{print $4" "$5}
-     sqrt( ($4+2.0)^2 + ($5+0.5)^2 ) < 0.2 {printf "\033[42m%s\033[m\n", "OK";exit(0)}
-     NR==1000{printf "\033[41m%s\033[m\n", "TIMEOUT";exit(1)}'
+     sqrt( ($4+2.0)^2 + ($5+0.5)^2 ) < 0.2 {printf "\033[42m%s\033[m\n", "ESTIMATION RECOVERY TEST OK";exit(0)}
+     NR==1000{printf "\033[41m%s\033[m\n", "ESTIMATION RECOVERY TEST TIMEOUT";exit(1)}'
 
 if [ "$?" -ne 0 ]; then
   exit 1
@@ -39,31 +39,27 @@ fi
 
 ros2 service call /global_costmap/clear_entirely_global_costmap nav2_msgs/srv/ClearEntireCostmap request:\ {}\ 
 
-sleep 10
+sleep 20
 
 ### NAVIGATION TEST ###
 # Publish nav2 goal
-ros2 topic pub /goal_pose geometry_msgs/msg/PoseStamped "
-header:
-  stamp:
-    sec: 0
-    nanosec: 0
-  frame_id: 'map'
+ros2 action send_goal /navigate_to_pose nav2_msgs/action/NavigateToPose "
 pose:
-  position: {x: -0.5, y: -0.5, z: 0.0}
-  orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}" | head -n 5
+  header: 
+    stamp: 
+      sec: 0
+    frame_id: 'map'
+  pose:
+    position: { x: -0.5, y: -0.5, z: 0.0}
+    orientation: { x: 0.0, y: 0.0, z: 0.0, w: 1.0}" &
 
 # Check if position is within a threshold
 ros2 topic echo /mcl_pose --csv | 
 head -n 1000 |
 awk -F',' '{print $4" "$5}
-     sqrt( ($4+0.5)^2 + ($5+0.5)^2 ) < 0.3 {printf "\033[42m%s\033[m\n", "OK";exit(0)}
-     NR==1000{printf "\033[41m%s\033[m\n", "TIMEOUT";exit(1)}'
+     sqrt( ($4+0.5)^2 + ($5+0.5)^2 ) < 0.3 {printf "\033[42m%s\033[m\n", "NAVIGATION TEST OK";exit(0)}
+     NR==1000{printf "\033[41m%s\033[m\n", "NAVIGATION TEST TIMEOUT";exit(1)}'
 
 RESULT=$?
-
-if [ "$?" -ne 0 ]; then
-  exit 1
-fi
 
 exit $RESULT
