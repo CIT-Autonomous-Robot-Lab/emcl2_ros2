44ae838be5ff894be5d71eacff2c97767f1648e7
makotoyoshigoe <myoshigo0127@gmail.com>
2024-04-15 20:27:29 +0900

    Update launch

--- launch/emcl2.launch.py
+++ launch/emcl2.launch.py
@@ -12,41 +12,63 @@ def generate_launch_description():
     params_file = LaunchConfiguration('params_file')
     map_yaml_file = LaunchConfiguration('map')
     use_sim_time = LaunchConfiguration('use_sim_time')
+    map_topic_name = LaunchConfiguration('map_topic_name')
+    namespace = LaunchConfiguration('namespace')
 
     declare_map_yaml = DeclareLaunchArgument(
         'map',
-        default_value='',
+        default_value=[
+            TextSubstitution(text=os.path.join(
+                get_package_share_directory('emcl2'), 'config', 'map', 'map_turtlebot_house.yaml'))],
         description='Full path to map yaml file to load')
+    
     declare_use_sim_time = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='false',
+        default_value='true',
         description='Use simulation (Gazebo) clock if true')
+    
     declare_params_file = DeclareLaunchArgument(
         'params_file',
         default_value=[
             TextSubstitution(text=os.path.join(
-                get_package_share_directory('emcl2'), 'config', '')),
-            TextSubstitution(text='emcl2.param.yaml')],
+                get_package_share_directory('emcl2'), 'config', 'param', 'emcl2.param.yaml'))],
         description='emcl2 param file path')
+    
+    declare_map_topic_name = DeclareLaunchArgument(
+        'map_topic_name', 
+        default_value=TextSubstitution(text='/map'), 
+        description='map topic name'
+    )
+    
+    declare_namespace = DeclareLaunchArgument(
+        'namespace', 
+        default_value=TextSubstitution(text='')
+    )
 
     lifecycle_nodes = ['map_server']
-
+    emcl2_map_topic = (namespace, '/', map_topic_name)
     launch_node = GroupAction(
         actions=[
             SetParameter('use_sim_time', use_sim_time),
             Node(
+                namespace=namespace,
                 package='nav2_map_server',
                 executable='map_server',
                 name='map_server',
                 parameters=[{'yaml_filename': map_yaml_file}],
-                output='screen'),
+                output='screen', 
+                remappings=[('map', map_topic_name)]
+                ),
             Node(
                 name='emcl2',
                 package='emcl2',
                 executable='emcl2_node',
                 parameters=[params_file],
-                output='screen'),
+                output='screen', 
+                remappings=[('map', emcl2_map_topic)]
+                ),
             Node(
+                namespace=namespace, 
                 package='nav2_lifecycle_manager',
                 executable='lifecycle_manager',
                 name='lifecycle_manager_localization',
@@ -57,9 +79,11 @@ def generate_launch_description():
     )
 
     ld = LaunchDescription()
+    ld.add_action(declare_map_topic_name)
     ld.add_action(declare_map_yaml)
     ld.add_action(declare_use_sim_time)
     ld.add_action(declare_params_file)
+    ld.add_action(declare_namespace)
 
     ld.add_action(launch_node)
 
---
+++ launch/emcl2_tsudanuma_219.launch.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2023 Makoto Yoshigoe myoshigo0127@gmail.com
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import TextSubstitution
+
+
+def generate_launch_description():    
+    tsudanuma_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            TextSubstitution(text=os.path.join(
+                    get_package_share_directory('emcl2'), 
+                    'launch', 'emcl2.launch.py'))]),
+        launch_arguments={
+            'map': [
+                TextSubstitution(text=os.path.join(
+                        get_package_share_directory('emcl2'), 
+                        'config', 'map', 'map_tsudanuma_campus.yaml'))],
+            'map_topic_name': [
+                TextSubstitution(text='/map/localization')
+            ]
+        }.items()
+    )
+    ld = LaunchDescription()
+    ld.add_action(tsudanuma_launch)
+    
+    return ld
